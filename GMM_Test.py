a72f3a7be635e41f165428b5bff537e27bbf7b57
sammytheindi <samyak.shahfamily@gmail.com>
2020-01-24 17:52:27 -0500

    Updated to include initializations and covariance

--- GMM_Test.py
+++ GMM_Test.py
@@ -2,13 +2,13 @@ from GMM import GMM
 from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
 
-X, y = make_blobs(n_samples=1000, centers=3, n_features=2)
+X, y = make_blobs(n_samples=1000, centers=4, n_features=2)
 
-gmm_cls = GMM()
+gmm_cls = GMM(initializer='uniform', cov_type='diag')
 gmm_cls.fit(X, 4)
 
 colors = []
-for l in gmm_cls.kmeans_cls_.memberships_:
+for l in gmm_cls.kmeans_cls_.predict(X):
     if l == 0:
         colors.append('red')
     if l == 1:
@@ -22,6 +22,6 @@ plt.scatter(X[:,0], X[:,1], c=colors, alpha=0.1)
 plt.scatter(gmm_cls.means_[:, 0], gmm_cls.means_[:, 1], c='k')
 plt.show()
 
-plt.scatter(X[:,0], X[:,1], c=colors, alpha=0.2)
+plt.scatter(X[:,0], X[:,1], c=colors, alpha=0.1)
 plt.scatter(gmm_cls.kmeans_cls_.means_[:, 0], gmm_cls.kmeans_cls_.means_[:, 1], c='k')
 plt.show()
